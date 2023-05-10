eb897f8cdac79131c5d99bcbb7642b77ca21e49e
MyunghaCho <chomyungha@gmail.com>
2023-05-10 09:00:13 +0900

    cors 추가

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: [
+        'https://port-0-aure-be-4c7jj2blhfis6sk.sel4.cloudtype.app',
+        'https://localhost:3000',
+      ],
+      Credential: true,
+    }),
+  );
 
   await app.listen(3000);
 }
